849aa80c3d49d2309198b9a58e6f9baf415d5192
pigluz <franciszeklatawiec@gmail.com>
2023-01-16 21:25:31 +0100

    add sign-up page

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ var email = document.getElementById("email");
 var div = document.getElementsByClassName("cointainer")
 
 document.getElementById("log_in").addEventListener("click", function login() {
-    document.getElementById("wrong").innerHTML = "We're sorry, there's no registered account on <br><b>" + email.value + "</b>!";
+    document.getElementById("info").innerHTML = "We're sorry, there's no registered account on <br><b>" + email.value + "</b>!";
 })
 
+document.getElementById("sign_up2").addEventListener("click", function signup() {
+    document.getElementById("info").innerHTML = "Account registered on <br><b> " + email.value + "</br></b>!";
 
+})
 
 
 function togglePass() {
