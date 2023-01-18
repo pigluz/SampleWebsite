05b3c08dcdd7fac3e8249d361d14cd707e2875f9
pigluz <franciszeklatawiec@gmail.com>
2023-01-18 21:29:07 +0100

    Fix javascript

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var email = document.getElementById("email");
 
-//log in script
-document.getElementById("login").addEventListener("click", function login() {
-    document.getElementById("info").innerHTML = "We're sorry, there's no registered account on <br><b>" + email.value + "</b>!";
-})
-
 // sign up script
-document.getElementById("signupp").addEventListener("click", function signupp() {
+const signup = document.getElementById("signupp");
+if (signup) signup.addEventListener("click", function (e) {
   document.getElementById('info').innerHTML = "Account registered on <br><b>" + email.value + "</b>!";
-})
+});
+
+// log in script
+const login = document.getElementById("login");
+if (login) login.addEventListener("click", function(e) {
+    document.getElementById("info").innerHTML = "We're sorry, there's no registered account on <br><b>" + email.value + "</b>!";
+});
+// fix found my mklkj :crying_face::crying_face::crying_face:
+
 
 // password toggle script
 function togglePass() {
